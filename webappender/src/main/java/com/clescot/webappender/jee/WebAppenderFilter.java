741f16279de3e181872f9bc952accd58bad1c0f4
Charles LESCOT <lescot.charles@gmail.com>
2014-03-16 20:03:57 +0100

    fix levelFilters feature.

--- webappender/src/main/java/com/clescot/webappender/jee/WebAppenderFilter.java
+++ webappender/src/main/java/com/clescot/webappender/jee/WebAppenderFilter.java
@@ -29,6 +29,12 @@ public class WebAppenderFilter implements Filter {
     public static final String X_VERBOSE_LOGS = "X-verbose-logs";
     public static final String X_THRESHOLD_FILTER = "X-threshold-filter";
     public static final String X_LEVEL_FILTER = "X-level-filter";
+    public static final String LEVEL_FILTER_SEPARATOR = ",";
+    public static final String LEVEL_FILTER_PROPERTY_SEPARATOR = ";";
+    public static final String FITLER_LEVEL_MATCH_PROPERTY = "MATCH";
+    public static final String FILTER_LEVEL_MISMATCH_PROPERTY = "MISMATCH";
+    public static final String FILTER_LEVEL_LEVEL_PROPERTY = "LEVEL";
+    public static final String KEY_VALUE_SEPARATOR = ":";
     private static Logger LOGGER = LoggerFactory.getLogger(WebAppenderFilter.class);
 
     private LogCollector logCollector;
@@ -105,33 +111,43 @@ public class WebAppenderFilter implements Filter {
         Enumeration<String> headers = httpServletRequest.getHeaders(X_LEVEL_FILTER);
         while (headers.hasMoreElements()) {
             String valueElement = headers.nextElement();
-            LevelFilter levelFilter = new LevelFilter();
-            List<String> values = Arrays.asList(valueElement.split(","));
+            List<String> values = Arrays.asList(valueElement.split(LEVEL_FILTER_SEPARATOR));
             for (String value : values) {
-                if (value.startsWith("MATCH")) {
-                    String firstValueTrimmed = getFirstValueTrimmed(value);
-                    if (Iterables.contains(Arrays.asList("NEUTRAL","ACCEPT","NEUTRAL"),firstValueTrimmed)){
-                        levelFilter.setOnMatch(FilterReply.valueOf(firstValueTrimmed));
-                    }
-                } else if (value.startsWith("MISMATCH")) {
-                    String firstValueTrimmed = getFirstValueTrimmed(value);
-                    if (Iterables.contains(Arrays.asList("NEUTRAL","ACCEPT","NEUTRAL"),firstValueTrimmed)){
-                        levelFilter.setOnMismatch(FilterReply.valueOf(firstValueTrimmed));
-                    }
-                } else if (value.startsWith("LEVEL")) {
-                    String firstValueTrimmed = getFirstValueTrimmed(value);
-                    levelFilter.setLevel(Level.toLevel(firstValueTrimmed));
-                }
+                LevelFilter levelFilter = getLevelFilter(value);
+                levelFilter.start();
+                levelFilters.add(levelFilter);
             }
 
-            levelFilter.start();
-            levelFilters.add(levelFilter);
         }
         return levelFilters;
     }
 
+    private LevelFilter getLevelFilter(String valueElement) {
+        LevelFilter levelFilter = new LevelFilter();
+        List<String> values = Arrays.asList(valueElement.split(LEVEL_FILTER_PROPERTY_SEPARATOR));
+        List<String> filterReplyValues = Arrays.asList(FilterReply.NEUTRAL.toString(), FilterReply.ACCEPT.toString(), FilterReply.DENY.toString());
+        for (String value : values) {
+            if (value.trim().startsWith(FITLER_LEVEL_MATCH_PROPERTY)) {
+                String firstValueTrimmed = getFirstValueTrimmed(value);
+
+                if (Iterables.contains(filterReplyValues, firstValueTrimmed)){
+                    levelFilter.setOnMatch(FilterReply.valueOf(firstValueTrimmed));
+                }
+            } else if (value.trim().startsWith(FILTER_LEVEL_MISMATCH_PROPERTY)) {
+                String firstValueTrimmed = getFirstValueTrimmed(value);
+                if (Iterables.contains(filterReplyValues,firstValueTrimmed)){
+                    levelFilter.setOnMismatch(FilterReply.valueOf(firstValueTrimmed));
+                }
+            } else if (value.trim().startsWith(FILTER_LEVEL_LEVEL_PROPERTY)) {
+                String firstValueTrimmed = getFirstValueTrimmed(value);
+                levelFilter.setLevel(Level.toLevel(firstValueTrimmed));
+            }
+        }
+        return levelFilter;
+    }
+
     private String getFirstValueTrimmed(String value) {
-        List<String> matchValues = Arrays.asList(value.split(":"));
+        List<String> matchValues = Arrays.asList(value.split(KEY_VALUE_SEPARATOR));
         return matchValues.get(1).trim();
     }
 
